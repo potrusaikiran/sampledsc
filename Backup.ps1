3a5b2880746f2fe995bf7dddd115056ab55e1ea3
Saikiran Potru <saikiran.potru@gmail.com>
2018-05-29 18:28:24 +0530

    Update Backup.ps1

--- Backup.ps1
+++ Backup.ps1
@@ -10,16 +10,3 @@
                         }
                     } 
 }
-
-
-                            backup -OutputPath C:\Users\SaiKiran\Desktop\Partial\Backup
-  Publish-DscConfiguration C:\Users\SaiKiran\Desktop\Partial\Backup -ComputerName 'localhost' -Verbose
-  Publish-DscConfiguration -Path C:\Users\SaiKiran\Desktop\Partial\Wireless-Networking -ComputerName 'localhost' -Verbose
- Start-DscConfiguration -Path C:\Users\SaiKiran\Desktop\Partial\Backup  -Verbose
- Get-WinEvent –LogName "Microsoft-Windows-Dsc/Operational" |? -process {Timecreated.Date=(Get-Date).Date}
- Get-DscLocalConfigurationManager
- Remove-DscConfigurationDocument -Stage Pending,Current,Previous
- Test-DscConfiguration
-
- get-process *wmi* | stop-process -force;
-restart-service winrm -force
