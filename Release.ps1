1034432ea529c8d6b2a5005898b003b1aa8ca818
Saikiran Potru <saikiran.potru@gmail.com>
2018-05-30 04:29:51 +0000

    Update Release.ps1

--- Release.ps1
+++ Release.ps1
@@ -1,3 +1,4 @@
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName skplte-RG
 $AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Backup.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName backup
+#edited for continuous build check
