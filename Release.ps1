fb9fb1708cc14fee53c139f7622401eabe965cbb
Saikiran Potru <saikiran.potru@gmail.com>
2018-05-30 04:52:02 +0000

    Update Release.ps1

--- Release.ps1
+++ Release.ps1
@@ -1,4 +1,5 @@
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName skplte-RG
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Backup.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Wireless-Networking.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName backup
 #edited for continuous build check
