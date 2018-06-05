8e4c7aa24978277935fe6bb81a987fc505223884
Saikiran Potru <saikiran.potru@gmail.com>
2018-06-05 08:58:34 +0000

    Update Release.ps1

--- Release.ps1
+++ Release.ps1
@@ -1,17 +1,17 @@
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\vmautomationbuild\webserver.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-vmdsc\vmdsc\webserver.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName webserver
 
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\vmautomationbuild\Backup.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-vmdsc\vmdsc\Backup.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName backup
 
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\vmautomationbuild\WirelessNetwork.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-vmdsc\vmdsc\WirelessNetwork.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName WirelessNetwork
 
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\vmautomationbuild\service.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-vmdsc\vmdsc\service.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName service
 #edited for continuous build check
 #added comment
