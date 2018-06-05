152c1e513e7698358c1e8a4055d5cdc021be7a49
Saikiran Potru <saikiran.potru@gmail.com>
2018-06-05 07:09:54 +0000

    Update Release.ps1

--- Release.ps1
+++ Release.ps1
@@ -1,17 +1,17 @@
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Backup.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\DSC\Backup.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName webserver
 
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Backup.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\DSC\Backup.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName backup
 
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\WirelessNetwork.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\DSC\WirelessNetwork.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName WirelessNetwork
 
 $AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
-$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\service.ps1" -Published -Force
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\vmautomationbuild\DSC\service.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName service
 #edited for continuous build check
 #added comment
