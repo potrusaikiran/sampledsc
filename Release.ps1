1516a4abdc349b007fd84aae4aa013f826a2283c
Saikiran Potru <saikiran.potru@gmail.com>
2018-06-05 06:55:04 +0000

    Update Release.ps1

--- Release.ps1
+++ Release.ps1
@@ -1,12 +1,16 @@
-$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName skplte-RG
+$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
+$AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Backup.ps1" -Published -Force
+$AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName webserver
+
+$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
 $AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\Backup.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName backup
 
-$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName skplte-RG
+$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
 $AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\WirelessNetwork.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName WirelessNetwork
 
-$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName skplte-RG
+$AutomationAccount=Get-AzureRmAutomationAccount -ResourceGroupName saikiranpotru
 $AutomationAccount|Import-AzureRmAutomationDscConfiguration -SourcePath "$env:System_artifactsdirectory\ES-DevOps-CI-CD-failure\DSC\service.ps1" -Published -Force
 $AutomationAccount|Start-AzureRmAutomationDscCompilationJob -ConfigurationName service
 #edited for continuous build check
