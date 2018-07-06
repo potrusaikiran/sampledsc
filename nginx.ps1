d91fb96a9046fd2797a257242da1c4310ca1d256
Saikiran Potru <saikiran.potru@gmail.com>
2018-07-06 15:09:36 +0530

    Create nginx.ps1

---
+++ nginx.ps1
@@ -0,0 +1,10 @@
+   sudo apt-get update -y 
+      sudo apt-get install nginx -y 
+      cd /var/www/html 
+      wget https://sl556.blob.core.windows.net/sample/index.zip 
+      apt-get install unzip 
+     unzip index.zip
+     #sudo apt-get remove nginx nginx-common
+     Install-Module PSDesiredStateConfiguration -Force
+     Install-Module nx -Force
+     Install-Module xPSDesiredStateConfiguration -Force
