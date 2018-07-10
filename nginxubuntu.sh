a8e55a4b83911ff2de49d88a8ffd9b9f2d5090c9
Saikiran Potru <saikiran.potru@gmail.com>
2018-07-10 10:07:40 +0530

    Update nginxubuntu.sh

--- nginxubuntu.sh
+++ nginxubuntu.sh
@@ -1,18 +1,18 @@
 
 #updating modules and install nginx
 sudo apt-get update -y   
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.8/powershell_6.0.0-beta.8-1.ubuntu.16.04_amd64.deb
-sudo dpkg -i ./powershell_6.0.0-beta.8-1.ubuntu.16.04_amd64.deb
-sudo apt-get install -y powershell
-apt-get -f install -y
+#wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.8/powershell_6.0.0-beta.8-1.ubuntu.16.04_amd64.deb
+#sudo dpkg -i ./powershell_6.0.0-beta.8-1.ubuntu.16.04_amd64.deb
+#sudo apt-get install -y powershell
+#apt-get -f install -y
 # Download and install the OMI
-wget https://github.com/Microsoft/omi/releases/download/v1.1.0-0/omi-1.1.0.ssl_100.x64.deb
-sudo dpkg -i ./omi-1.1.0.ssl_100.x64.deb
+#wget https://github.com/Microsoft/omi/releases/download/v1.1.0-0/omi-1.1.0.ssl_100.x64.deb
+#sudo dpkg -i ./omi-1.1.0.ssl_100.x64.deb
 #Download and install the DSC 
-wget https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_100.x64.deb
-sudo dpkg -i ./dsc-1.1.1-294.ssl_100.x64.deb
+#wget https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_100.x64.deb
+#sudo dpkg -i ./dsc-1.1.1-294.ssl_100.x64.deb
 #Restart OMI
-sudo /opt/omi/bin/service_control restart
+#sudo /opt/omi/bin/service_control restart
 #configuration modules
 #sudo powershell
 sudo apt-get update -y 
