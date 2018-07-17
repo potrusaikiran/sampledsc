dc571fb6df2d60caf976096cd41195a81fb00815
Saikiran Potru <saikiran.potru@gmail.com>
2018-07-17 17:48:08 +0530

    Create newrhel.sh

---
+++ newrhel.sh
@@ -0,0 +1,8 @@
+mkdir /home/install 
+cd /home/install 
+wget http://nginx.org/packages/rhel/7/noarch/RPMS/nginx-release-rhel-7-0.el7.ngx.noarch.rpm 
+rpm -i nginx-release-rhel-7-0.el7.ngx.noarch.rpm 
+yum install nginx -y 
+systemctl enable nginx 
+systemctl start nginx
+systemctl status nginx
